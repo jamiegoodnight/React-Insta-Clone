1544fbb5dac4fabac6bf8b150e149c20226387d9
Jamie Goodnight <jamiegoodnight@Jamies-Air.attlocal.net>
2019-02-18 15:08:34 -0500

    Accidentally used folders instead of files before. Fixed. Building app.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+
+import dummyData from './dummy-data'
 import './App.css';
 
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      dummy=dummyData,
+    }
+  }
+
   render() {
     return (
       <div className="App">
+      <SearchBar 
 
+      />
+      <PostContainer 
+
+      />
+      <CommentSection 
+
+      />
       </div>
     );
   }
